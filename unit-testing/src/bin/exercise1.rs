53a22c8e8120ab280a0846a2be244444d93cacaf
Roey Prat <roey.prat@redislabs.com>
2020-05-04 20:10:54 +0300

    quickcheck

--- unit-testing/src/bin/exercise1.rs
+++ unit-testing/src/bin/exercise1.rs
@@ -1,10 +1,27 @@
-use ::byte_strings::concat_bytes;
-
 #[macro_use(quickcheck)]
 extern crate quickcheck_macros;
 
+use std::str;
+
 #[derive(Debug, PartialEq)]
-struct RespError {}
+struct RespError <'a>{
+    explanation: &'a str
+}
+
+fn parse_bulk(data: &[u8]) -> Result<&[u8], RespError> {
+    let mut num_chars: u64 = 0;
+    for ch in data[1..(data.len() - 1)] {
+        if ch.is_digit(10) {
+            num_chars *= 10;
+            num_chars += ch.to_digit().unwrap();
+        } else {
+            break;
+        }
+    }
+    if num_chars == 0 {
+        return Err(RespError { explanation: "could not parse num of chars" })
+    }
+}
 
 fn resp_parse(data: &[u8]) -> Result<&[u8], RespError> {
     match data[0] {
@@ -14,20 +31,20 @@ fn resp_parse(data: &[u8]) -> Result<&[u8], RespError> {
         b'$' => {
             Ok(data)
         }
-        _ => Err(RespError {})
+        _ => Err(RespError { explanation: "not valid because of first char" })
     }
 }
 
-#[test]
-fn test_invalid_input() {
-    assert_eq!(Err(RespError {}), resp_parse(b"-not valid because of first char\r\n"));
+#[quickcheck]
+fn test_invalid_input(expected: String) -> bool {
+    //
+    let unparsed = format!("-{}\r\n", expected);
+    Err(RespError { explanation: "not valid because of first char" }) == resp_parse(unparsed.as_bytes())
 }
 
-#[test]
-fn test_simple_string() {
-    let expected: &[u8] = b"so simple";
-    let unparsed = concat_bytes!(b"+", expected, b"\r\n");
-    let actual = resp_parse(unparsed).unwrap();
-    assert_eq!(expected.len(), actual.len(), "Arrays don't have the same length");
-    assert!(expected.iter().zip(actual.iter()).all(|(a, b)| a == b), "Arrays are not equal");
+#[quickcheck]
+fn simple_string(expected: String) -> bool {
+    let unparsed = format!("+{}\r\n", expected);
+    let actual = resp_parse(unparsed.as_bytes()).unwrap();
+    expected == str::from_utf8(actual).unwrap()
 }
