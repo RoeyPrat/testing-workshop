08fee29860ae21d0f8e6462af994f42e0f87792d
Roey Prat <roey.prat@redislabs.com>
2020-05-20 19:41:10 +0300

    integers

--- exercises/src/main.rs
+++ exercises/src/main.rs
@@ -5,6 +5,12 @@ use std::str;
 
 use quickcheck::{Arbitrary, Gen};
 
+use rand::{
+    self,
+    distributions::{Distribution, Uniform},
+}; // 0.6.4
+
+
 const NEWLINE: &[u8] = b"\r\n";
 
 #[derive(Debug, PartialEq)]
@@ -23,6 +29,7 @@ enum RespError {
 enum RedisValue<'data> {
     SimpleString(&'data [u8]),
     BulkString(&'data [u8]),
+    Integer(i64),
     Null,
 }
 
@@ -30,6 +37,7 @@ enum RedisValue<'data> {
 enum RedisValueOwned {
     SimpleString(Vec<u8>),
     BulkString(Vec<u8>),
+    Integer(i64),
     Null,
 }
 
@@ -37,6 +45,7 @@ fn resp_parse(data: &[u8]) -> Result<RedisValue, RespError> {
     match &data {
         [b'+', data @ ..] => parse_simple_string(data),
         [b'$', data @ ..] => parse_bulk_string(data),
+        [b':', data @ ..] => parse_integer(data),
         _ => Err(RespError::InvalidData),
     }
 }
@@ -78,6 +87,17 @@ fn parse_bulk_string(data: &[u8]) -> Result<RedisValue, RespError> {
     }
 }
 
+fn parse_integer(data: &[u8]) -> Result<RedisValue, RespError> {
+    match split_line(data) {
+        (Some(line), _) => {
+            let string = str::from_utf8(line).map_err(|_| RespError::InvalidData)?;
+            let value: i64 = string.parse().map_err(|_| RespError::InvalidData)?;
+            Ok(RedisValue::Integer(value))
+        }
+        (None, _) => Err(RespError::MissingEndOfLine),
+    }
+}
+
 fn split_line(data: &[u8]) -> (Option<&[u8]>, &[u8]) {
     find_subsequence(data, NEWLINE)
         .map(|i| {
@@ -96,8 +116,24 @@ fn find_subsequence(haystack: &[u8], needle: &[u8]) -> Option<usize> {
 
 impl Arbitrary for RedisValueOwned {
     fn arbitrary<G: Gen>(g: &mut G) -> Self {
-        let data: Vec<u8> = Arbitrary::arbitrary(g);
-        RedisValueOwned::BulkString(data)
+        let mut rng = rand::thread_rng();
+        let die_range = Uniform::new_inclusive(0, 3);
+        let die = die_range.sample(&mut rng);
+        match die {
+            0 => {
+                let data: Vec<u8> = Arbitrary::arbitrary(g);
+                RedisValueOwned::BulkString(data)
+            }
+            1 => {
+                let data: Vec<u8> = Arbitrary::arbitrary(g);
+                RedisValueOwned::SimpleString(data)
+            }
+            2 => RedisValueOwned::Null,
+            _ => {
+                let data: i64 = Arbitrary::arbitrary(g);
+                RedisValueOwned::Integer(data)
+            }
+        }
     }
 }
 
@@ -122,24 +158,54 @@ fn qc_roundtrip_simple_string(input: RedisValueOwned) -> bool {
                 _ => false,
             }
         }
-        RedisValueOwned::SimpleString(s) => todo!("bulk string"),
-        RedisValueOwned::Null => todo!("null"),
+        RedisValueOwned::SimpleString(input_data) => {
+            let mut resp = vec![];
+            resp.extend_from_slice(b"+");
+            resp.extend_from_slice(input_data.as_slice());
+            resp.extend_from_slice(b"\r\n");
+            eprintln!("Testing Simple String with length {}", input_data.len());
+            eprintln!("RESP: {:?}", String::from_utf8_lossy(&resp));
+            let value = resp_parse(resp.as_slice()).expect("valid RESP data");
+            match value {
+                RedisValue::SimpleString(parsed_data) => parsed_data == input_data.as_slice(),
+                _ => false,
+            }
+        }
+        RedisValueOwned::Integer(input_data) => {
+            let mut resp = vec![];
+            resp.extend_from_slice(b":");
+            resp.extend_from_slice(input_data.to_string().as_str().as_bytes());
+            resp.extend_from_slice(b"\r\n");
+            eprintln!("Testing Integer");
+            eprintln!("RESP: {:?}", String::from_utf8_lossy(&resp));
+            let value = resp_parse(resp.as_slice()).expect("valid RESP data");
+            match value {
+                RedisValue::Integer(parsed_data) => parsed_data == input_data,
+                _ => false,
+            }
+        },
+        _ => {
+            let value = resp_parse(b"$-1\r\n").expect("valid RESP data");
+            RedisValue::Null == value
+        },
     }
 }
 
 fn assert_parse_eq(input: &[u8], expected: &RedisValue) {
     let parsed = &resp_parse(input).unwrap();
 
-    let expected_str = match expected {
-        RedisValue::SimpleString(s) => str::from_utf8(s).unwrap(),
-        RedisValue::BulkString(s) => str::from_utf8(s).unwrap(),
-        RedisValue::Null => "(nil)",
+    let expected_str: String = match expected {
+        RedisValue::SimpleString(s) => String::from(str::from_utf8(s).unwrap()),
+        RedisValue::BulkString(s) => String::from(str::from_utf8(s).unwrap()),
+        RedisValue::Integer(i) => format!("{}", i),
+        RedisValue::Null => String::from("(nil)"),
     };
 
     let parsed_str = match parsed {
-        RedisValue::SimpleString(s) => str::from_utf8(s).unwrap(),
-        RedisValue::BulkString(s) => str::from_utf8(s).unwrap(),
-        RedisValue::Null => "(nil)",
+        RedisValue::SimpleString(s) => String::from(str::from_utf8(s).unwrap()),
+        RedisValue::BulkString(s) => String::from(str::from_utf8(s).unwrap()),
+        RedisValue::Integer(i) => format!("{}", i),
+        RedisValue::Null => String::from("(nil)"),
     };
 
     assert_eq!(
